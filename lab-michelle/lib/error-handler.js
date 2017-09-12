ed9ffacbb58a1ed5696b014f40b40806ed7dab73
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-09-11 22:19:00 -0700

    finished scaffold

--- lab-michelle/lib/error-handler.js
+++ lab-michelle/lib/error-handler.js
@@ -8,6 +8,10 @@ module.exports = function(err, req, res) {
   let msg = err.message.toLowerCase();
 
   switch(true) {
-    case msg.includes('authorization failed'): return res.status(401).send(`${err.name}: ${err.message}`);
-  }
-}
+    case msg.includes('authorization failed'): return res.status(401).send(`${err.name}: ${err.message}`)
+    msg.includes('validation failed'): return res.status(400).send(`${err.name}: ${err.message}`)
+    msg.includes('duplicate key'): return res.status(409).send(`${err.name}: ${err.message}`)
+    msg.includes('objectid failed'): return res.status(404).send(`${err.name}: ${err.message}`)
+    default: return res.status(500).send(`${err.name}: ${err.message}`)
+  };
+};
